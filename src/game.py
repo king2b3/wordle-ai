ea9409f8789c64b99b9d97200e511955ff3e02b3
Bayley King <king2b3@mail.uc.edu>
2022-02-14 22:35:29 -0500

    text based version complete
    Only working in OS X, linux, and windows powershell using ANISCON. See https://stackoverflow.com/questions/53629115/colored-text-output-on-powershell for options for powershell. Going to create AI version, which can call, OR going to create powershell. TODO keyboard on screen. No guess penalty for non-wrods

---
+++ src/game.py
@@ -0,0 +1,73 @@
+import random, pickle as pkl
+from collections import defaultdict
+wordList = pkl.load( open( "words.p", "rb" ) )
+
+bcolors = {
+    'GREEN': '\033[92m',
+	'YELLOW': '\033[93m',
+    'ENDC': '\033[0m',
+}
+
+
+
+def def_value():
+    return 0
+      
+class Game():
+	def __init__(self):
+		self.word = random.choice(wordList)
+		self.letters = defaultdict(def_value)
+		for l in self.word:
+			self.letters[l] += 1
+		#print(self.word)
+	
+	def checkword(self, guess) -> list:
+		temp = ""
+		temp_dict = defaultdict(def_value)
+		if guess == self.word:
+			return "win"
+		else:
+			for l in range(len(guess)):
+				temp_dict[guess[l]] += 1
+				if guess[l] == self.word[l]:
+					temp += f"{bcolors['GREEN']}{guess[l]}{bcolors['ENDC']}"
+				elif guess[l] in self.word:
+					if temp_dict[l] > self.letters[l]:
+						temp += f"{guess[l]}"
+					else:
+						temp += f"{bcolors['YELLOW']}{guess[l]}{bcolors['ENDC']}"					
+				else:
+					temp += f"{guess[l]}{bcolors['ENDC']}"
+			return temp
+	
+	def check(self, guess):
+		if len(guess) != 5:
+			print(f"Invalid word")
+			return None
+		if guess in wordList:
+			return self.checkword(guess)
+		else:
+			print(f"Not a word")
+			return None
+
+
+import os
+def clearScreen():
+	os.system('cls' if os.name == 'nt' else 'clear')
+
+game = Game()
+history = f"Welcome to Wordle\n"
+for turn in range(1,7):
+	clearScreen()
+	print(history)
+	print(f"turn:{turn}")
+	g = input()
+	g.strip()
+	if (temp := game.check(g)) == "win":
+		print(f"YOU WIN")
+		exit()
+	if temp:
+		history += temp + '\n'
+print(f"THE WORD IS {game.word}")
+print(f"YOU LOSE. SAD")
+
